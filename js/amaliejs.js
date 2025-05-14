e77d7fac1b1c1c71d6d73f6d7d9c6731ee1fc2d1
amaliekea <amje0001@stud.kea.dk>
2025-05-14 09:29:57 +0200

    init

---
+++ js/amaliejs.js
@@ -0,0 +1,16 @@
+console.log("jeg er i amalie.js");
+
+function hentPersoner() {
+    fetch("http://localhost:8080/api/persons")
+        .then(response => response.json())
+        .then(data => {
+            const liste = document.getElementById("personListe");
+            liste.innerHTML = "";
+            data.forEach(person => {
+                const li = document.createElement("li");
+                li.textContent = person.name;
+                liste.appendChild(li);
+            });
+        })
+        .catch(err => console.error("Fejl ved hentning:", err));
+}
